d9f9c41db3c8e9b2786a015a5ad3b8ee4f4c1d69
Juan Navarro <juanavarroquintero@gmail.com>
2023-09-04 22:47:57 -0500

    Aplica el Join Today

--- src/main/java/userinterface/JoinPage.java
+++ src/main/java/userinterface/JoinPage.java
@@ -6,5 +6,5 @@ import org.openqa.selenium.By;
 
 public class JoinPage extends PageObject {
     public static final Target JOIN_BUTTON = Target.the("button to create new account").
-            located(By.className("unauthenticated-nav-bar__dropdown-menu-sign-up"));
+            located(By.className("unauthenticated-nav-bar__sign-up"));
 }
